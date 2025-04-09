27b3295d6aa694483fb2cab3673bda058b9fe210
Jaydeep-sardar <jaydeep.sardar.official@gmail.com>
2025-04-09 21:09:39 +0530

    updating

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,17 @@ import AboutPage from "./pages/About";
 import ContactPage from "./pages/Contact";
 import PolicyPage from "./pages/Policy";
 import PageNotFound from "./pages/PageNotFound";
+import register from "./pages/Auth/register";
+import login from "./pages/Auth/login";
+import Dashboard from "./pages/auth/dashboard";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<PolicyPage />} />
+        <Route path="/register" element={<PolicyPage />} />
+        <Route path="/login" element={<PolicyPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/policy" element={<PolicyPage />} />
---
+++ client/src/pages/user/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import layout from '../../components/layout/Layout'
+
+const Dashboard = () => {
+  return (
+    <layout title={'Dashboard -Ecommerce app'}>
+      <h1>dashboard page</h1>
+    </layout>
+  )
+}
+
+export default Dashboard
