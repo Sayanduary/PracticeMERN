0b515650936beebd54323ed7f9124eefe8ca8c3e
Sayanduary <sayanduary@gmail.com>
2025-04-09 20:12:16 +0530

    done

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -4,28 +4,28 @@ import {NavLink} from 'react-router-dom'
 function Header() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
     </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <NavLink to ='/'class="navbar-brand" href="#">Hidden brand</NavLink>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <NavLink to ='/'class="nav-link active" aria-current="page" href="#">Home</NavLink>
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <navlink to="/" className="navbar-brand" href="#">Hidden brand</navlink>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <navlink to="/" className="nav-link active" aria-current="page" href="#">Home</navlink>
         </li>
-        <li class="nav-item">
-          <NavLink to ='/'class="nav-link" href="#">Link</NavLink>
+        <li className="nav-item">
+          <navlink to="/" className="nav-link" href="#">Link</navlink>
         </li>
-        <li class="nav-item">
-          <NavLink to ='/'class="nav-link disabled" aria-disabled="true">Disabled</NavLink>
+        <li className="nav-item">
+          <navlink to="/" className="nav-link disabled" aria-disabled="true">Disabled</navlink>
         </li>
       </ul>
-      
     </div>
   </div>
 </nav>
+
     </>
   );
 }
