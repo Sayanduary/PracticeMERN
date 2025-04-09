813a80f19e6c084b9f8ab3fc51e9a70e67016ee3
Sayanduary <sayanduary@gmail.com>
2025-04-09 20:11:14 +0530

    done

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,9 +1,19 @@
-import Header from './Header';
-import Footer from './Footer';
+import Header from "./Header";
+import Footer from "./Footer";
+import { Helmet } from "react-helmet";
 
-function Layout({ children }) {
+function Layout({ children, title, description, keywords, author }) {
   return (
     <div className="min-h-screen flex flex-col">
+      <Helmet>
+        <div>
+          <meta charSet="utf-8" />
+          <meta name="description" content="Farmer" />
+          <meta name="keywords" content="HTML, CSS, JavaScript" />
+          <meta name="author" content="John Doe" />
+          <title>My Title</title>
+        </div>
+      </Helmet>
       <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
