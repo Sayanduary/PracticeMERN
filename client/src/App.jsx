760b683ebaa028e785759f7da6c91c7fa99e8a28
Sayanduary <sayanduary@gmail.com>
2025-04-09 19:43:00 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from './pages/Home'
+import HomePage from "./pages/Home";
+import AboutPage from "./pages/About";
+import { ContactPage } from "./pages/Contact";
+import {PolicyPage} from './pages/Policy'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={ContactPage} />
+        <Route path="/policy" element={PolicyPage}/>
+        <Route pa/>
       </Routes>
     </>
   );
