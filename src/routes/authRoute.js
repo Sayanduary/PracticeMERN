4526cba5e0213166d4351c65b1db9efbe75c1bde
Sayanduary <sayanduary@gmail.com>
2025-04-10 07:46:05 +0530

    done

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -17,7 +17,7 @@ router.post('/register', registerController);
 router.post("/login", loginController);
 
 //forgot password
-router.post("/forgot-password", forgotPasswordController)
+// router.post("/forgot-password", forgotPasswordController)
 
 //test routes
 router.get("/test", requireSignIn, isAdmin, testController);
